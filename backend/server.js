3f593e98cf89ce6152612647f24c1766d1bb43d6
sasindu <sasindusachintha1234@gmail.com>
2026-06-07 19:35:58 +0530

    file structure

--- backend/server.js
+++ backend/server.js
@@ -2,7 +2,7 @@ require("dotenv").config();
 
 const express = require("express");
 const cors = require("cors");
-const growthRoutes = require("./routes/growth");
+const growthRoutes = require("./backend/routes/growth");
 
 const app = express();
 const PORT = process.env.PORT || 3000;
